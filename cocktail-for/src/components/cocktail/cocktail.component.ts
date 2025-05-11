58b801f8303ed5f50fefcb64527d0bebeaf9e93f
dyksai <dyksai30@gmail.com>
2025-05-11 16:46:29 +0530

    removed unused css

--- cocktail-for/src/components/cocktail/cocktail.component.ts
+++ cocktail-for/src/components/cocktail/cocktail.component.ts
@@ -14,6 +14,7 @@ export class CocktailComponent implements OnInit {
   constructor(private cocktailService: CocktailService , private router: Router,  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.filterType = 'Alcoholic';
     this.route.queryParams.subscribe(params => {
       const cocktailName = params['name'];
       if (cocktailName) {
